551fe1cc75669efba1aa04867690cc0b2baa38c8
Aryan Karan <aryankaran28022004@gmail.com>
2024-10-29 13:08:14 +0530

    Lab : Sep 24, 2024

---
+++ circular.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *next;
+};
+
+typedef struct Node *Nodeptr;
+typedef struct Node list;
+
+
+int main() {
+    
+}

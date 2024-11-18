3b65ba0bd7d88a7b7929c322ceb055e6d022273b
Aryan Karan <aryankaran28022004@gmail.com>
2024-11-18 23:55:56 +0530

    Lab @ 2024-09-18 14:39

---
+++ lab.py
@@ -0,0 +1,46 @@
+name = "aRyan kAran"
+
+print(name.title())
+print(name.lower())
+print(name.upper())
+
+name1 = "Hey"
+
+print(name1 + "\t" + name.title())
+
+name1.strip()
+
+
+# Program to convet given chartacters into ascii value
+
+y = input()
+print(y)
+
+
+# ASCII value conversion
+
+y_ascii = ord(y)
+y_char = chr(y_ascii)
+print(y_ascii)
+print(y_char)
+
+
+# modulo
+
+y1 = -14 % 3
+y2 = -14 % -3
+y3 = 14 % 3
+
+print(y1, y2, y3)
+
+
+# exponential
+
+y = 2**3
+
+
+
+m = 5
+y = ++m + m++
+
+print(m, y)

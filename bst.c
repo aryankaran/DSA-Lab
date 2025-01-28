a5e596333e4d56450bb6f5bf307746406b86c153
Aryan Karan <aryankaran28022004@gmail.com>
2025-01-28 12:48:27 +0530

    BST updte @ Jan 28, 2025: 12:48
     Changes to be committed:
    	modified:   bst
    	modified:   bst.c
    	new file:   lab-Dec11-loops.py
    
      BST functions: search, inorder successor

--- bst.c
+++ bst.c
@@ -68,7 +68,7 @@ Nodeptr createBST() {
     return Tree;
 }
 
-
+/*
 void deleteBST(Nodeptr Tree, int element) {
     Nodeptr x = Tree;
     Nodeptr y = NULL;
@@ -104,7 +104,7 @@ void deleteBST(Nodeptr Tree, int element) {
 
 
 }
-
+*/
 
 
 /*
@@ -143,8 +143,103 @@ void printBST_VLR(Nodeptr Tree) {
     }
 }
 
+/*
+Nodeptr searchBST(Nodeptr Tree, int z) {
+    Nodeptr y = NULL;
+    Nodeptr x = Tree;
+
+    // searching
+    while (x != NULL && x->data != z->data) {
+        y = x;
+
+        if (z < x->data) {
+            x = x->left;
+        } else {
+            x = x->right;
+        }
+    }
+    
+    if (NULL != x && z == x->data){
+        return x;
+    } else {
+        return NULL;
+    }
+}
+*/
+
+Nodeptr searchBST(Nodeptr T, int z) {
+    Nodeptr x = T;
+    while (x != NULL && x->data != z) {
+        if (z < x->data) {
+            x = x->left;
+        } else {
+            x = x->right;
+        }
+    }
+
+    return x;
+}
 
 
+Nodeptr minimum(Nodeptr T) {
+    Nodeptr x = T;
+
+    while (x->left != NULL) {
+        x = x->left;
+    }
+
+    return x;
+}
+
+Nodeptr maximum(Nodeptr T, int* counter) {
+    Nodeptr x = T;
+
+    while (x->right != NULL) {
+        x = x->right;
+        (*counter)++;
+        printf("DEBUG: counter value %d\n", *counter);
+    }
+
+    return x;
+}
+
+
+
+Nodeptr inorderSuccessor(Nodeptr Tree, int z) {
+    // search element in Tree firstly
+    Nodeptr k = searchBST(Tree, z);
+    
+/*
+    if (k != NULL){
+        printf("DEBUG: searched element %d\n", k->data);
+    } else {
+        printf("DEBUG: node not found...\n");
+    }
+*/
+
+    if (k == NULL) {
+        printf("Node not found!!\n");
+        return k;
+    } else {
+        if (k->right != NULL) {
+            Nodeptr m = minimum(k->right);
+            return m;
+        } else {
+            Nodeptr succ = NULL;
+            Nodeptr T1 = Tree;
+
+            while (T1 != k) {
+                if (z < T1->data) {
+                    succ = T1;
+                } else {
+                    T1 = T1->right;
+                }
+            }
+            return succ;
+        }
+    }
+
+}
 
 
 int main() {
@@ -162,17 +257,30 @@ int main() {
     printBST_VLR(Tree);
     printf("\n");
 
+/*
     int element;
     printf("Enter element to delete: ");
     scanf("%d", &element);
     // z = getnode();
     // z->data = element;
     deleteBST(Tree, element);
+*/
 
     printf("Tree in LVR  after deletion: ");
     printBST_LVR(Tree);
     printf("\n");
 
+    // int search = 3;
+    int searching_element;
+    printf("Enter element to seach: ");
+    scanf("%d", &searching_element);
+
+    Nodeptr in_succ = inorderSuccessor(Tree, searching_element);
+    printf("Inorder successor of %d is %d\n", searching_element, in_succ->data);
+
+    int counter = 0;
+    Nodeptr max = maximum(Tree, &counter);
+    printf("Maximum element %d, height: %d\n", max->data, counter);
 
     return 0;
 }
---
+++ lab-Dec11-loops.py
@@ -0,0 +1,13 @@
+''' 
+implement do while loop using python syntax
+
+
+- for loop
+- while loop
+- do-while loop
+
+'''
+
+do :
+    something 
+while ()

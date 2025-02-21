2e942d934fd4b1ef02002eea0b967d3d6631031c
Aryan Karan <aryankaran28022004@gmail.com>
2025-02-21 23:53:17 +0530

    Infix to Postfix completed @ Feb 21, 2025: 23:53
       taught on Feb 11, 2025
    
     Changes to be committed:
    	modified:   infix-to-postfix
    	modified:   infix-to-postfix.c
    
       in 1:45 mins with tracedown on stack functions
       Life update: idk what's going on with ranita literally
                    I love her so much & gonna propose her now ...❤️❤️❤️

--- infix-to-postfix.c
+++ infix-to-postfix.c
@@ -2,18 +2,122 @@
 
 #define MAXSIZE 40
 
-void in_to_post(char* expr[]) {
-    int top = -1;
+
+int priority(char x) {
+    switch(x) {
+        case '^':
+            return 4;
+        
+        case '*':
+        case '/':
+            return 3;
+        
+        case '+':
+        case '-':
+            return 2;
+        
+        case '(':
+            return 0;
+
+        default:
+            return -1;
+    }
+}
+
+
+int is_operand(char opr){
+    if ( (opr >= 'a' && opr <= 'z') || (opr >= 'A' && opr <= 'Z') || (opr >= '0' && opr <= '9') ) {
+        return 1;
+    } else return 0;
+}
+
+
+void push(char stack[], int* top, int maxsize, char ch){
+    if (!(*top >= maxsize - 1)) {
+        stack[++(*top)] = ch;
+    } else {
+        printf("Stack overflow !!!\n");
+        return;
+    }
+}
+
+
+char pop(char stack[], int* top){
+    if (*top < 0){
+        printf("Stack underflow !!!\n");
+        return '\0';
+    }
+
+    // (*top)--;
+    return stack[(*top)--]; // return popped pointer
+}
+
+
+void in_to_post(char expr[]) {
     printf("Expression: %s\n", expr);
-    
+
+    char stack[MAXSIZE]; // intermediate array as stack
+    int top = -1;
+
+    for (int i = 0; expr[i] != '\0'; i++) {
+        if (is_operand(expr[i])) {
+            printf("%c", expr[i]);
+
+        } else if (expr[i] == '(') {
+            // opening paranthesis case
+            push(stack, &top, MAXSIZE, expr[i]);
+
+        } else if (expr[i] == ')') {
+            // closing paranthesis, then pop till opening comes in
+            while (stack[top] != '(') {
+                char y = pop(stack, &top);
+                printf("%c", y);
+            }
+            
+            // pop out the '(' from stack before proceeding
+            pop(stack, &top);
+            
+        } else { // other operator handling
+            while (priority(stack[top]) >= priority(expr[i])) {
+                printf("%c", pop(stack, &top)); // pop & print until stack[top] has lower prior then expr[i]
+            }
+            push(stack, &top, MAXSIZE, expr[i]);
+        }
+    }
+
+    // remaining operators if any
+    while (top > -1) {
+        printf("%c", pop(stack, &top));
+    }
+
+    printf("\n");
+
+/*
+    printf("Expression line by line: \n");
+    int i = 0;
+    while (expr[i] != '\0') {
+        printf("%c\n", expr[i++]);
+    }
+*/ 
 }
 
 int main() {
     char expr[MAXSIZE];
     printf("Enter expression: ");
-    scanf("%s", expr);
+    scanf("%[^\n]", expr);
     in_to_post(expr);
-    // printf("Expression: %s\n", expr);
+
+/*
+    int top = -1;
+    for (int i = 0; expr[i] != '\0'; i++, top++);
+    printf("Top: %d\n", top);
+
+    // int top = -1;
+    for (int i = 0; expr[i] != '\0'; i++) { 
+        printf("Before: %d:-> %c, ", i, expr[i]);
+        printf("Popped element: %c\n", pop(expr, &top));
+    }
+*/
 }
 
 
